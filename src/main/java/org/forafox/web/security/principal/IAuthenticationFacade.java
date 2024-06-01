97ac15efda565e8e382b4e35c7e8597ccbb8e468
forafox <deskpa@yandex.ru>
2024-06-01 22:12:31 +0300

    feat: add AuthenticationFacade

---
+++ src/main/java/org/forafox/web/security/principal/IAuthenticationFacade.java
@@ -0,0 +1,9 @@
+package org.forafox.web.security.principal;
+
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+    Authentication getAuthentication();
+
+    String getAuthName();
+}
